33d72be7ec712cf71ccd6f2d79c5d1dc5aa8258b
madou <laheen@gmail.com>
2017-10-22 09:51:42 +1100

    chore(i18n): adds missing profession translations

--- src/components/CharacterPreview/_stories.js
+++ src/components/CharacterPreview/_stories.js
@@ -5,4 +5,5 @@ import App from '../../../stories/App';
 import CharacterPreview from './';
 
 storiesOf('CharacterPreview', module)
-  .add('default', () => <App><CharacterPreview name="Quartermile" /></App>);
+  .add('default', () => <App><CharacterPreview name="Quartermile" /></App>)
+  .add('elite spec', () => <App><CharacterPreview name="Blastrn" /></App>);
